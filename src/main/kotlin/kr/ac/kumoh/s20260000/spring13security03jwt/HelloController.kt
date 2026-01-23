b4066a8eba36384bf4249ee8019ac94f45a83ac1
prog4th <prog4th@proton.me>
2026-01-24 06:16:40 +0900

    ✨@AuthenticationPrincipal

--- src/main/kotlin/kr/ac/kumoh/s20260000/spring13security03jwt/HelloController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring13security03jwt/HelloController.kt
@@ -1,6 +1,8 @@
 package kr.ac.kumoh.s20260000.spring13security03jwt
 
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,13 +10,20 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
     @GetMapping("/hello")
     fun hello(authentication: Authentication): String {
-        // (방법 1) 이렇게 꺼내는 것이 편하고 일반적임
         val username = authentication.name
 
-        // (방법 2) 이렇게 principal을 통해서 꺼내도 되긴 함
+        // 이렇게 principal을 통해서 꺼내도 되긴 함
 //        val userDetails = authentication.principal as UserDetails
 //        val username = userDetails.username
 
         return "$username 님, 안녕하세요.  <a href='/logout'>로그아웃</a>"
+    }
+
+    // @AuthenticationPrincipal 사용 (권장)
+    @GetMapping("/user")
+    fun getUser(
+        @AuthenticationPrincipal user: UserDetails
+    ): String {
+        return "username: ${user.username}  <a href='/logout'>로그아웃</a>"
     }
 }
